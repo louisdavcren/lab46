e586ba086726a57ceb8317cd0ff6a27be8b557ad
louisdavcren <lejdc15@gmail.com>
2018-10-19 10:22:40 +0100

    Lab exercises 4 and 6

---
+++ game2_template/items.py
@@ -0,0 +1,63 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+
+    "mass": 1,
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?..."""
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "name": "laptop",
+
+    "mass": 3,
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!"
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+
+    "mass": 1,
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees."
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "name": "a pack of biscuits",
+
+    "mass": 1,
+
+    "description": "A pack of biscuits."
+}
+
+item_pen = {
+    "id": "pen",
+    
+    "name": "a pen",
+
+    "mass": 1,
+
+    "description": "A basic ballpoint pen."
+}
+
+item_handbook = {
+    "id": "handbook",
+    
+    "name": "a student handbook",
+    
+    "mass": 2,
+
+    "description": "This student handbook explains everything. Seriously."
+}
